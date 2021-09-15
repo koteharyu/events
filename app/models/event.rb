e6697a0a87006260193ed58b2543ca5a00bdd063
haryu <hryau6@gmail.com>
2021-09-15 17:16:53 +0900

    add: associations and validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,7 @@ class Event < ApplicationRecord
   validate :start_at_should_be_before_end_at
 
   belongs_to :owner, class_name: 'User'
+  has_many :tickets
 
   scope :not_held_events, -> { where('start_at > ?', Time.zone.now).order(:start_at) }
 
