1818c18fffca6ca21a13a00fce30153f05830e0a
haryu <hryau6@gmail.com>
2021-09-15 16:50:31 +0900

    add: events/edit and update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,6 +21,15 @@ class EventsController < ApplicationController
     @event = current_user.created_events.find(params[:id])
   end
 
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, success: 'udpated a event'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def event_params
