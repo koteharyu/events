10594410a8b7296dc1ef5a6ab2042f3789e5d432
haryu <hryau6@gmail.com>
2021-09-16 11:29:17 +0900

    add: 退会機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :events do
     resources :tickets
   end
+  resource :retirements
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
