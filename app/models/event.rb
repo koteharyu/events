05f2f9f695cbede13fd534e3e93708019c7b08c4
haryu <hryau6@gmail.com>
2021-09-16 14:10:19 +0900

    add: setup searchkick

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   attr_accessor :remove_image
+  searchkick language: "japanese"
   before_save :remove_image_if_user_accept
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -35,5 +36,15 @@ class Event < ApplicationRecord
 
   def remove_image_if_user_accept
     self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
+  end
+
+  def search_data
+    {
+      name: name,
+      place: place,
+      content: content,
+      owner_name: owner&.name,
+      start_at: start_at
+    }
   end
 end
