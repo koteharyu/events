46c1a980f951bd68c5079f85cd92da7e2baa3bc8
haryu <hryau6@gmail.com>
2021-09-16 12:24:52 +0900

    add: display a event' image in events/show

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  attr_accessor :remove_image
+  before_save :remove_image_if_user_accept
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :place, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 2000 }
@@ -7,7 +10,7 @@ class Event < ApplicationRecord
   validate :start_at_should_be_before_end_at
 
   belongs_to :owner, class_name: 'User'
-  has_one_attached :image
+  has_one_attached :image, dependent: false
   has_many :tickets, dependent: :destroy
 
   scope :not_held_events, -> { where('start_at > ?', Time.zone.now).order(:start_at) }
@@ -26,5 +29,9 @@ class Event < ApplicationRecord
       errors[:start_at] << "は終了時間よりも前にしてください"
       # errors.add(:start_at, "は終了時間よりも前にしてください")
     end
+  end
+
+  def remove_image_if_user_accept
+    image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
   end
 end
